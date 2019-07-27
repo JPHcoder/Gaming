65061a6bade17eb7ff0481b2ca67598f8b74bfd2
JPHcoder <52888443+JPHcoder@users.noreply.github.com>
2019-07-27 08:37:34 -0400

    Update snake.js

--- js/snake/js/snake.js
+++ js/snake/js/snake.js
@@ -613,7 +613,7 @@ SNAKE.Board = SNAKE.Board || (function() {
             
             elmAboutPanel = document.createElement("div");
             elmAboutPanel.className = "snake-panel-component";
-            elmAboutPanel.innerHTML = "<a href='https://github.com/JPHcoder/Gaming/edit/master/js/snake/js' class='snake-link'>source code</a>";
+            elmAboutPanel.innerHTML = "<a href='https://github.com/JPHcoder/Gaming/edit/master/js/snake' class='snake-link'>source code</a>";
             
             elmLengthPanel = document.createElement("div");
             elmLengthPanel.className = "snake-panel-component";
